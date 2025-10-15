afa3f3a2a0ddb5a2d2920a49944e5cd09401974c
Nguyen Thanh Luan <imthanhluan203@gmail.com>
2025-10-15 10:30:33 +0700

    logic select is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,26 @@ import { Player } from "./components/Player"
 import { Board } from "./components/Board"
 import { useState } from "react"
 function App() {
+  const [playerTurn,updatePlayerTurn] = useState([]);
   const [player,updatePlayer] = useState('player1');
-  function handleSelect(){
-    player === 'player1' ? updatePlayer('player2') : updatePlayer('player1');
+  function handleSelect(rowIndex,colIndex){
+    let repeat = false;
+    playerTurn.forEach(({row,col,player})=>{
+        if(row === rowIndex && col === colIndex){
+          repeat = true;
+        }
+    })
+    if(repeat === false){
+      player === 'player1' ? updatePlayer('player2') : updatePlayer('player1');
+      updatePlayerTurn((current)=>{
+        let current_player = 'player1';
+        if(current.length > 0 && current[0].player === 'player1'){
+          current_player = 'player2';
+        }
+        const newState = [{row:rowIndex,col:colIndex,player:current_player},...playerTurn];
+        return newState;
+      })
+    }
   }
   return (
     <main>
@@ -13,7 +30,7 @@ function App() {
           <Player addClass ={player === 'player1'? 'active' : undefined} name="Player1" symbol="X"/>
           <Player addClass ={player === 'player2'? 'active' : undefined} currentPlayer={player} name="Player2" symbol="O"/>
         </ol>
-        <Board currentPlayer={player} onSelect = {()=>handleSelect('player1')}/>
+        <Board currentPlayer={playerTurn}  onSelect = {(rowIndex,colIndex)=>handleSelect(rowIndex,colIndex)}/>
       </div>
     </main>
   )
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,25 +1,20 @@
-import { useState } from "react"
 const initialBoard = [
     [null,null,null],
     [null,null,null],
     [null,null,null],
 ]
 export function Board({onSelect,currentPlayer}){
-    const [currentBoard,updateBoard] = useState(initialBoard);
-    function handleClick(i,j){
-        onSelect();
-        updateBoard((currentBoard)=>{
-                const copy_board = [...currentBoard.map((x)=>[...x])];
-                copy_board[i][j] = currentPlayer === 'player1' ? 'X' : 'O';
-                return copy_board;
-        })
-    }
+    const currentBoard = initialBoard;
+    currentPlayer.forEach(({row,col,player})=>{
+        const sign = player === 'player1' ? 'X' : 'O';
+        currentBoard[row][col] = sign;
+    })
     return <ol id="game-board">
         {currentBoard.map((row,indexRow)=>{
             return <li key={indexRow}>
                 <ol>
                     {row.map((col,indexCol)=>{
-                        return <li key={indexCol}><button onClick={()=>{handleClick(indexRow,indexCol)}}>{col}</button></li>
+                        return <li key={indexCol}><button onClick={()=>{onSelect(indexRow,indexCol)}}>{col}</button></li>
                     })}
                 </ol>
             </li>
